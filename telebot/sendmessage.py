df27faddeed03cd1df5bb9ad0c4b29008088a62c
AxL <axlmalyasov@gmail.com>
2021-09-07 17:15:43 +1000

    после оставлени заявки в telegram chat шлетс сообщение

--- telebot/sendmessage.py
+++ telebot/sendmessage.py
@@ -1,10 +1,15 @@
 import requests
+from .models import TeleSettings
 
-token = '1991124690:AAENy2gDNu9gSGWEsrDePyl2TI7bpPoyKe4'
-chat_id = '-510990311'
-text = "Моё тестовое сообщение"
+# token = '1991124690:AAENy2gDNu9gSGWEsrDePyl2TI7bpPoyKe4'
+# chat_id = '-510990311'
+# text = "Моё тестовое сообщение"
 
 def sendTelegram():
+    settings = TeleSettings.objects.get(pk=1)
+    token = str(settings.tg_token)
+    chat_id = str(settings.tg_chat)
+    text = str(settings.tg_message)
     api = 'https://api.telegram.org/bot'
     method = api + token + '/sendMessage'
 
@@ -13,4 +18,3 @@ def sendTelegram():
         'text': text
     })
 
-sendTelegram()
